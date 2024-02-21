cbe0c315cca1a910080481baf4f5f901a6ec832e
Sw1ft0 <pasha.k460@gmail.com>
2024-02-21 22:02:07 +0000

    Version 2

--- main.py
+++ main.py
@@ -4,25 +4,17 @@ command12 = "d12"
 
 
 def roll_dice(command: str) -> str:
-    match command:
-
-        case 'd4':
-            return str(random.randint(1, 5))
-
-        case 'd6':
-            return str(random.randint(1, 7))
-
-        case 'd8':
-            return str(random.randint(1, 9))
-
-        case 'd10':
-            return str(random.randint(1, 11))
-
-        case 'd12':
-            return str(random.randint(1, 13))
-
-        case _:
-            return "Error. Wrong command."
+    d_counter: int = 0
+    num: str = ''
+    symb: str
+    for symb in command:
+        if symb.isdigit() and d_counter == 1:
+            num = num + symb
+        elif symb == 'd':
+            d_counter = d_counter + 1
+        else:
+            return f"Error. Wrong input: only 1 \'d\' letter possible but it is {d_counter}"
+    return 'Result: ' + str(random.randint(1, int(num)))
 
 print(roll_dice(command12))
 
