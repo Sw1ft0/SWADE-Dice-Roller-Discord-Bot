8b50eccfb937c7ad19f937c9ca82824952b99900
Sw1ft0 <pasha.k460@gmail.com>
2024-03-27 23:03:29 +0000

    make a structure for state machine

--- tokenizer.py
+++ tokenizer.py
@@ -9,16 +9,32 @@ class Token(NamedTuple):
     position: int
 
 
-keywords = {'d', 'D', 'dice', 'DICE'}
+class Edge(NamedTuple):
+    beginning_point: int
+    token_type: str
+    end_point: int
+
+
+edge_list: list[Edge] = [
+    Edge(0, 'SKIP', 0),
+    Edge(0, 'NUMBER', 1),
+    Edge(0, 'KEYWORD', 2),
+    Edge(1, 'KEYWORD', 2),
+    Edge(2, 'NUMBER', 3),
+    Edge(3, '+', 0),
+    Edge(3, 'SKIP', 3),
+    Edge(3, 'EOE', 4),
+]
+print(edge_list)
 
 
 def tokenize(command):
     token_specification = [
-        ('NUMBER',   r'\d+'),   # Integer number
-        ('WORD',       r'[A-Za-z]+'),     # Identifiers
-        ('OP',       r'[+\-*/]'),       # Arithmetic operators
-        ('SKIP',     r'[ \t]+'),        # Skip over spaces and tabs
-        ('MISMATCH', r'.'),             # Any other character
+        ('NUMBER', r'\d+'),  # Integer number
+        ('KEYWORD', r'dice|DICE|d|D'),  # Identifiers
+        ('OP', r'[+\-*/]'),  # Arithmetic operators
+        ('SKIP', r'[ \t]+'),  # Skip over spaces and tabs
+        ('MISMATCH', r'.'),  # Any other character
     ]
     tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
     line_start = 0
@@ -26,13 +42,7 @@ def tokenize(command):
         kind: str | None = mo.lastgroup
         value: int | str = mo.group()
         position = mo.start() - line_start
-        if kind == 'NUMBER':
-            value = int(value)
-        elif kind == 'WORD' and value in keywords:
-            kind = value
-        elif kind == 'WORD':
-            raise ValueError(f'{value!r} unexpected')
-        elif kind == 'SKIP':
+        if kind == 'SKIP':
             continue
         elif kind == 'MISMATCH':
             raise RuntimeError(f'{value!r} unexpected')
@@ -45,7 +55,7 @@ def roll_with_tokenizer(command):
     quantity: int = 1
     result_line: str = f'Result of rolling {command} is '
     for i, token in enumerate(x):
-        if token.type in keywords:
+        if token.type == 'KEYWORD':
             keyword_indicator = True
         elif token.type == 'NUMBER' and keyword_indicator:
             for j in range(quantity):
